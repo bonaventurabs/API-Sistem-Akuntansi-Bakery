9ba8d8bf5b2d70a611d85faecb54a88a579c38dc
navent <18219017@std.stei.itb.ac.id>
2021-11-21 19:34:58 +0700

    modify database

--- src/database.py
+++ src/database.py
@@ -3,15 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# USER = "nspdsyefuxriqm"
-# PASSWORD = "1532f0eea628e84e3bddf102e09920722e0f7f5020a3224350f0d2d533bae05a"
-# HOST = "ec2-44-199-40-188.compute-1.amazonaws.com"
-# DATABASE = "dfiumv6bioofku"
+USER = "nspdsyefuxriqm"
+PASSWORD = "1532f0eea628e84e3bddf102e09920722e0f7f5020a3224350f0d2d533bae05a"
+HOST = "ec2-44-199-40-188.compute-1.amazonaws.com"
+DATABASE = "dfiumv6bioofku"
 
-USER = "postgres"
-PASSWORD = "bgs280901"
-HOST = "localhost"
-DATABASE = "test1rpl"
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
 
